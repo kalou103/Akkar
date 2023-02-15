9aa28f5643227c962d1e3dfeb82c329c42647e07
MohamedAmineBenSalah213 <MohamedAmineBenSalah213>
2023-02-15 21:31:40 +0100

    amine commit

--- src/main/java/com/example/akkar2/services/IClientService.java
+++ src/main/java/com/example/akkar2/services/IClientService.java
@@ -1,11 +1,10 @@
 package com.example.akkar2.services;
 
-import org.springframework.stereotype.Service;
 import com.example.akkar2.entities.Client;
 
 import java.util.List;
 
-@Service
+
 public interface IClientService {
     Client addUser(Client c);
 
